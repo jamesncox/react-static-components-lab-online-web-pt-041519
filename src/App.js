d0dc794604a79b5277757ef0bb2123e85ebf34ee
jamesncox <jamesncox@yahoo.com>
2020-01-08 13:09:35 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CatComponent from '.CatComponent.js'
-import { EinsteinQuoteComponent } from '.EinsteinQuoteComponent.js'
-import MouseComponent from '.MouseComponent.js'
+import CatComponent from './CatComponent.js'
+import { EinsteinQuoteComponent } from './EinsteinQuoteComponent.js'
+import MouseComponent from './MouseComponent.js'
 
 class App extends Component {
 	render() {
